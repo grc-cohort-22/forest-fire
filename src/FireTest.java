d6ff695a22d2d39cbe7f61bb71eaba60561b575d
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-04-25 09:28:23 -0700

    tested empty space between two tree

--- src/FireTest.java
+++ src/FireTest.java
@@ -54,5 +54,20 @@ public class FireTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testTimeToBurnBlockedByEmptySpace() {
+        char[][] forest = {
+        { 't', '.', 't' }
+    };
+
+        int matchR = 0;
+        int matchC = 0;
+
+        int expected = 0;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+}
+
     // Add more tests!
 }
