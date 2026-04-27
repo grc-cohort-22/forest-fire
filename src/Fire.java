e0b68141b18caf77883237d42c52058d4f6c36e8
konradkelly <konradky@gmail.com>
2026-04-27 14:16:39 -0700

    Implement solution for timeToBurn problem

--- src/Fire.java
+++ src/Fire.java
@@ -1,8 +1,11 @@
-
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
+import java.util.Set;
 
-public class Fire {
+public class Fire {    
     /**
      * Returns how long it takes for all vulnerable trees to be set on fire if a
      * match is lit at a given location.
@@ -39,40 +42,55 @@ public class Fire {
      * @param matchC The column the match is lit at
      * @return the time at which the final tree to be incinerated starts burning
      */
+
     public static int timeToBurn(char[][] forest, int matchR, int matchC) {
-        // HINT: when adding to your BFS queue, you can include more information than
-        // just a location. What other information might be useful?
+        Set<Integer> depthSet = new HashSet<>();
+        Location inputLocation = new Location(matchR, matchC, 0);
 
-        // Implement this AND add more tests!!!
-        // return -1;
+        Queue<Location> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
 
-        // Exceptions to make: 
-        // multiple fires
-        // no starting location
-        // no trees available
+        queue.add(inputLocation);
 
+        while (!queue.isEmpty()){
+            Location current = queue.poll();
+            String currentString = "" + current.row() + " " + current.col();
+            if (visited.contains(currentString)) continue;
+            
+            visited.add(currentString);
+            depthSet.add(current.depth());
 
-        // Location start = forestLocation(forest);
-        // Queue<Location> queue = new LinkedList<>()
-
-
-         public static Location forestLocation( char[][] forest) throws NoStartingLocationException, NoTreesAvailableException {
-            Location location = null;
-
-            for (int row = 0; row < forest.length; row++) {
-                for (int col = 0; col < forest[row].length; col++) {
-                    if (forest[row][col] == 't') {
-                        if (location == null) {
-                            location = new location (row, col);
-                        } else {
-                            throw new NoTreesAvailableException();
-                        }
-                    }
-                }
+            for (Location neighbor : neighbors(forest, current)){
+                queue.add(neighbor);
             }
-            if (location == null) {
-                throw new NoFiresException();
+        }
+        return depthSet.size() - 1;
+    }
+
+    public static List<Location> neighbors(char[][] forest, Location current){
+        int currentDepth = current.depth() + 1;
+        List<Location> result = new ArrayList<>();
+        int[][] moves = new int[][]{
+            {-1, 0}, //UP
+            {1, 0}, //DOWN
+            {0, -1}, //LEFT
+            {0, 1} //RIGHT
+        };
+
+        for (int[] move : moves){
+            int newR = current.row() + move[0];
+            int newC = current.col() + move[1];
+
+            if (
+                newR >= 0 && newR < forest.length && 
+                newC >= 0 && newC < forest[0].length &&
+                forest[newR][newC] == 't'
+            ){
+
+                result.add(new Location(newR, newC, currentDepth));
             }
-         }
+        }
+
+        return result;
     }
 }
