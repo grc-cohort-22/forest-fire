5cb74bd8b96c369b8799cb879e50a53e51842cc1
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-04-27 15:40:48 -0700

    added more tests

--- src/FireTest.java
+++ src/FireTest.java
@@ -122,4 +122,89 @@ public class FireTest {
         };
         assertEquals(3, Fire.timeToBurn(forest, 0, 0));
     }
+  @Test
+public void testMatchOnEmptyGround() {
+    char[][] forest = {
+        {'.', 't'},
+        {'t', 't'}
+    };
+
+    int matchR = 0;
+    int matchC = 0;
+
+    int expected = 0; // nothing burns
+    int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+    assertEquals(expected, actual);
+}  
+
+@Test
+public void testStartInMiddle() {
+    char[][] forest = {
+        {'t', 't', 't'},
+        {'t', 't', 't'},
+        {'t', 't', 't'}
+    };
+
+    int matchR = 1;
+    int matchC = 1;
+
+    int expected = 2; // center to corners
+    int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+    assertEquals(expected, actual);
+}
+
+@Test
+public void testSingleColumn() {
+    char[][] forest = {
+        {'t'},
+        {'t'},
+        {'t'},
+        {'t'},
+        {'t'}
+    };
+
+    int matchR = 0;
+    int matchC = 0;
+
+    int expected = 4;
+    int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+    assertEquals(expected, actual);
+}
+
+@Test
+public void testIsolatedTree() {
+    char[][] forest = {
+        {'.', '.', '.'},
+        {'.', 't', '.'},
+        {'.', '.', '.'}
+    };
+
+    int matchR = 1;
+    int matchC = 1;
+
+    int expected = 0;
+    int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+    assertEquals(expected, actual);
+}
+
+@Test
+public void testDiagonalNotConnected() {
+    char[][] forest = {
+        {'t', '.', 't'},
+        {'.', 't', '.'},
+        {'t', '.', 't'}
+    };
+
+    int matchR = 1;
+    int matchC = 1;
+
+    int expected = 0; // no neighboring trees
+    int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+    assertEquals(expected, actual);
+}
 }
